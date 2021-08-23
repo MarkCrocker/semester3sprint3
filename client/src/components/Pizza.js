80cba698bc962b0db3afaa230450c32c568527f5
MarkCrocker <71830279+MarkCrocker@users.noreply.github.com>
2021-08-23 12:22:00 -0230

    Update Pizza.js

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -65,7 +65,7 @@ export default function Pizza({ pizza }) {
 
       <div className="flex-container">
         <div className="m-1 w-100">
-          <h1 className="mt-1">Price: {pizza.prices[variant] * quantity}</h1>
+          <h1 className="mt-1">Price: {pizza.prices[0][variant] * quantity}</h1>
         </div>
         <div className="m-1 w-100">
           <button className="btn" onClick={addtocart}>
